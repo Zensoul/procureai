c7a1a01bbcce08b28ef7b0a631e7d0a9c28ab53e
Zensoul <zensoul.mitherapy@gmail.com>
2026-09-04 01:28:44 +0530

    chore: remove temp test file

--- src/procureai/test_config_temp.py
+++
@@ -1,7 +0,0 @@
-from procureai.config import get_settings
-s = get_settings()
-print("App:", s.app_name)
-print("Env:", s.app_env)
-print("Is dev:", s.is_development)
-print("Log level:", s.log_level)
-print("Config OK")
